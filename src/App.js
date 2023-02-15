15d853ac5c2ac2ee4be48b0ddd9330ac06327f84
John Doe <noabouskil@gmail.com>
2023-02-15 22:36:47 +0100

    commit1

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import InfiniteScroll from "./Components/InfiniteScroll/InfiniteScroll";
+
+
+function App() {
+  return (
+    <div>
+      <InfiniteScroll/>
+    </div>
+  );
+}
+
+export default App;
